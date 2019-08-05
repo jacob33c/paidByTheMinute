c413781e1e78a6ef5ca8ccd5623c3e0ecec9aced
Jacob Fraizer <jacob33c@gmail.com>
2019-08-04 21:00:18 -0700

    change style and button

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
 // IDEA: to know if button has been clicked already
 var started = false;
-
-
+var hourlyRate = 0;
+var myVar = 0;
+// IDEA: starting value is one penny
+var money = 0.01;
+var form = document.getElementById("rate");
 // IDEA: waits for someone to click the start button
 $(".myButton").on("click", function() {
   // IDEA: gets the hourly rate inputted by user
-  var hourlyRate = document.getElementById("rate").value;
-
+  hourlyRate = document.getElementById("rate").value;
+  //// IDEA: checkes to see if button is working
+  console.log("IVE BEEN CLICKED");
 // IDEA: checks to make sure button hasnt been clicked already and
 //that an hourlyRate has been inputted
   if (started == false && hourlyRate > 0 ) {
     // IDEA: gets the rate input element
-    var form = document.getElementById("rate");
+    start();
+  }
+  else stop();
 
-    // IDEA: this is how much the user will get per second
-    var increment = hourlyRate / 3600;
+});
 
-    // IDEA: starting value is one penny
-    var money = 0.01;
 
-    // IDEA: the button has now been CLICKED
-    started = true;
 
-    // IDEA: hides the rate form
-    form.style.display = "none";
 
-    //// IDEA: checkes to see if button is working
-    console.log("IVE BEEN CLICKED");
+function stop(){
+  console.log("stop function");
+  started = false;
+  $(".myButton").html("Start");
+  myVar = clearInterval(myVar);
+  form.classList.remove("hidden");
+}
 
-    //// IDEA: main func that increments the money every second and
-    //updates that on the screen for the userS
-    setInterval(function() {
+
+function start(){
+  console.log("start function");
+
+  // IDEA: this is how much the user will get per second
+  var increment = hourlyRate / 3600;
+
+  // IDEA: the button has now been CLICKED
+  started = true;
+  // IDEA: hides the rate form
+  form.classList.add("hidden");
+  //// IDEA: main func that increments the money every second and
+  //updates that on the screen for the userS
+  myVar  = setInterval(function() {
+      $(".myButton").html("stop");
       money += increment;
       var n = money.toFixed(2);
       document.getElementById("money").innerHTML = "$" + n;
     }, 1000);
-  }
-});
+}
